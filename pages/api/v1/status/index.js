6111a59a4727b9b8349a7f6c408e5f65185d172a
Willian Brusch <willian.bruschh@gmail.com>
2025-05-07 15:05:51 -0300

    add services scripts

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,7 +2,7 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result);
+  console.log(result.rows);
   response.status(200).json({ msg: "média" });
 }
 

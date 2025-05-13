add8d81bdcd8eb480989a6e729073982f82975b1
Willian Brusch <willian.bruschh@gmail.com>
2025-05-12 21:56:35 -0300

    add some results in payload status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,20 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ msg: "média" });
+  const updatedAt = new Date().toISOString();
+
+  const version = await database.query("SELECT version();");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const openedConnections = await database.query(
+    "SELECT COUNT(*) FROM pg_stat_activity;",
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    version: version.rows[0].version.slice(11, 15),
+    max_connections: maxConnections.rows[0]["max_connections"],
+    opened_connections: openedConnections.rows[0]["count"],
+  });
 }
 
 export default status;

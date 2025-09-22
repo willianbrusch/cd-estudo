9e8dabb78e9e84b3235f831b828f101de5dbd520
Willian Brusch <willian.bruschh@gmail.com>
2025-09-22 13:47:02 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,17 +3,24 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const version = await database.query("SELECT version();");
+  const version = await database.query("SHOW server_version;");
   const maxConnections = await database.query("SHOW max_connections;");
-  const openedConnections = await database.query(
-    "SELECT COUNT(*) FROM pg_stat_activity;",
-  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
 
   response.status(200).json({
     updated_at: updatedAt,
-    version: version.rows[0].version.slice(11, 15),
-    max_connections: maxConnections.rows[0]["max_connections"],
-    opened_connections: openedConnections.rows[0]["count"],
+    dependencies: {
+      database: {
+        version: version.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: openedConnections.rows[0].count,
+      },
+    },
   });
 }
 
